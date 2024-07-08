8497e96560c066f466d0ea6280a7596508f938eb
Irfan Fadillah <900522@hq.bni.co.id>
2024-07-08 16:16:51 +0700

    First Commit

---
+++ src/test/java/com/csv/belajar_java_csv/BelajarJavaCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csv.belajar_java_csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
